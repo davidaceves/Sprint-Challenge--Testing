14e17506f1a10cc9869a6dea8191f7b6f2a3ced9
David <digital0david@gmail.com>
2019-06-29 12:41:06 -0400

    dependencies

---
+++ games/gamesModel.js
@@ -0,0 +1,19 @@
+const db = require('../data/dbConfig.js');
+
+module.exports = {
+    insert,
+    getAll
+};
+
+async function insert(game) {
+    const [id] = await db('games').insert(game);
+    return findById(id);
+}
+
+function getAll() {
+    return db('games');
+}
+
+function findById(id) {
+    return db('games').where({ id }).first();
+}
