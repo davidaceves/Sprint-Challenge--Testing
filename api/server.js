f6a37b4c66aae8c63888593b11395206b35dbb10
David <digital0david@gmail.com>
2019-06-29 12:31:20 -0400

    file/folder structure + server setup

---
+++ api/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const Games = require('../games/gamesModel.js');
+
+const server = express();
+
+server.get('/games', (req, res) => {
+    
+})
+
+module.exports = server;
